36961a4f6f92cc1f6f5a92cf52a4201ef32420f0
mrbergis <mrbergis@mail.ru>
2023-04-23 23:21:28 +0300

    Strategy Pattern

---
+++ Assets/Patterns/SpecialAbilities/AbilityRunner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class AbilityRunner : MonoBehaviour {
+    [SerializeField] IAbility currentAbility = new RageAbility();
+
+    public void UseAbility()
+    {
+        currentAbility.Use(gameObject);
+    }
+}
+
+public interface IAbility 
+{
+    void Use(GameObject currentGameObject);
+}
+
+public class RageAbility : MonoBehaviour, IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("I'm always angry");
+    }
+}
+
+public class HealAbility : ScriptableObject, IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Here eat this!");
+    }
+}
+
+public class FireballAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Launch Fireball");
+    }
+}
