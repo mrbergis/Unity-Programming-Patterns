a11496b97300c3ba0cbd35e08ea0a1c90f601ddc
mrbergis <mrbergis@mail.ru>
2023-04-23 20:26:10 +0300

    Better Than A Singleton - Лучше, чем синглтон

--- Assets/Patterns/Singleton/AmbientAudioPlayerBehaviour.cs
+++ Assets/Patterns/Singleton/AmbientAudioPlayerBehaviour.cs
@@ -2,22 +2,6 @@ using UnityEngine;
 
 public class AmbientAudioPlayerBehaviour : MonoBehaviour
 {
-
-    private AmbientAudioPlayerBehaviour _instance = null;
-    
-    private void Awake() {
-        if (_instance == null)
-        {
-            _instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if( _instance != this)
-        {
-            Destroy(gameObject);
-        }
-        
-    }
-
     public void FadeNewMusic(AudioClip clip)
     {
         
